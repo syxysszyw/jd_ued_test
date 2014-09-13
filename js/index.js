b23d610b65de5bf6008e76250479ebdf77f66ac8
syxysszyw <545235009@qq.com>
2014-09-13 16:32:13 +0800

    v5

--- js/index.js
+++ js/index.js
@@ -1,41 +1,6 @@
-﻿		// var timer = index = 0, count, curIndex;
-		
-		// $(document).ready(function(){
-		// 	count = $('.banner_list a').length;
-		// 	$('.banner_list a:not(:first-child)').hide();
-		// 	$('.banner li').on('click', function(){
-		// 		var i = $(this).data('id') - 1;
-		// 		index = i;
-		// 		if(i >= count) return;
-		// 		$('.banner_list a').filter(':visible').fadeOut(200).parent().children().eq(i).fadeIn(1000);
-		// 		$('.banner').css('background', '');
-		// 		$(this).toggleClass('on').siblings().removeAttr("class");
-		// 	});
-
-		// 	$('#prev').on('click', function(){
-		// 		index = $('.banner_list a').filter(':visible').index();
-		// 		index = index <= 0 ? (count - 1) : --index;
-		// 		$('.banner li').eq(index).trigger('click');
-		// 	});
-
-		// 	$('#next').on('click', function(){
-		// 		index = $('.banner_list a').filter(':visible').index();
-		// 		index = index >= (count - 1)? 0 : ++index;
-		// 		$('.banner li').eq(index).trigger('click');
-		// 	});
-
-		// 	timer = setInterval('fShowAuto()', 2000);
-		// 	$('.banner').hover(function(){clearInterval(timer)}, function(){timer = setInterval('fShowAuto()', 2000);});
-		// });
-
-		// function fShowAuto(){
-		// 	index = index >= (count - 1)? 0 : ++index;
-		// 	$('.banner li').eq(index).trigger('click');
-		// }
-
+﻿		var timer = index = 0, count, curIndex;
 		$(function(){
-
-			/*ie67兼容before&after伪类*/
+			/*------------ie67兼容before&after伪类-----------*/
 			var $beforeAfter = function(dom) { 
 			if (document.querySelector || !dom && dom.nodeType !== 1) return; 
 				var content = dom.getAttribute("data-content") || ''; 
@@ -49,9 +14,34 @@
 				dom.appendChild(after); 
 			};  
 			$beforeAfter($('.service')[0]);
+			/* --------------------End------------------------- */
+			/*--------------------轮播图--------------------*/
+			count = $('.banner_list li').length;
+			$('.banner_list li:not(:first-child)').hide();
+			$('.banner-page-ul li').on('click', function(){
+				var i = $(this).data('id') - 1;
+				index = i;
+				if(i >= count) return;
+				$('.banner_list li').filter(':visible').fadeOut(200).parent().children().eq(i).fadeIn(1000);
+				$(this).toggleClass('on').siblings().removeAttr("class");
+			});
 
+			$('#prev').on('click', function(){
+				index = $('.banner_list li').filter(':visible').index();
+				index = index <= 0 ? (count - 1) : --index;
+				$('.banner-page-ul li').eq(index).trigger('click');
+			});
 
+			$('#next').on('click', function(){
+				index = $('.banner_list li').filter(':visible').index();
+				index = index >= (count - 1)? 0 : ++index;
+				$('.banner-page-ul li').eq(index).trigger('click');
+			});
 
+			timer = setInterval('showAuto()', 2000);
+			$('.banner-wrapper').hover(function(){clearInterval(timer)}, function(){timer = setInterval('showAuto()', 2000);});
+			/* --------------------End------------------------- */
+			/* --------------------产品切换-------------------- */
 			$('.product-top-page').on('click', function(evt){
 				var _this = $(evt.target);
 				if(_this.data('id')){
@@ -60,11 +50,9 @@
 					var target = _this.parent().parent().next().children().children().removeClass('productShow').eq(prodPageIndex-1).addClass('productShow');
 				}				
 			});
-
-			$('.banner-ul').on('click', function(evt){
-				var _this = $(evt.target);
-				if(_this.data('id')){
-					_this.addClass('on').siblings().removeAttr('class');
-				}				
-			});
+			/* --------------------End------------------------- */
 		});
+		function showAuto(){
+			index = index >= (count - 1)? 0 : ++index;
+			$('.banner-page-ul li').eq(index).trigger('click');
+		}
