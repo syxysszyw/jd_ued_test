9fb9dbd3b1a94566d01027a40333ed5ecd660596
JTCHEN <545235009@qq.com>
2014-09-14 10:06:56 +0800

    v8

--- js/index.js
+++ js/index.js
@@ -24,17 +24,20 @@
 				index = i;
 				if(i >= count) return;
 				$('.banner-wrapper').css('background', '');
-				$('.banner_list li').eq(i).fadeIn(4000).siblings().fadeOut(2000);
+				$('.banner_list li').eq(i).fadeIn(2000).siblings().fadeOut(1000);
 				$(this).toggleClass('on').siblings().removeAttr("class");
 			});
 
 			$('#prev').on('click', function(){
+				$('.banner_list li:animated').stop(true, true);
 				index = $('.banner_list li').filter(':visible').index();
+				console.log(index);
 				index = index <= 0 ? (count - 1) : --index;
 				$('.banner-page-ul li').eq(index).trigger('click');
 			});
 
-			$('#next').on('click', function(){
+			$('#next').on('click', function(){					
+				$('.banner_list li:animated').stop(true, true);
 				index = $('.banner_list li').filter(':visible').index();
 				index = index >= (count - 1)? 0 : ++index;
 				$('.banner-page-ul li').eq(index).trigger('click');
@@ -60,8 +63,7 @@
 			/* --------------------End------------------------- */
 			/*---------------------console output --------------*/
 
-			window.debug('面试官您好！我是来自广东外语外贸大学软件工程1101专业的陈洁婷，我希望成为一名前端开发工程师，谢谢您给我面试的机会！');
-			/*-------------------------End----------------------*/
+			window.debug('Dear JD面试官您好！我是来自广东外语外贸大学软件工程1101专业的陈洁婷o(∩_∩)o，谢谢您给我面试的机会，不管面试结果如何，我都希望有机会能和你们一起探讨一下我实现的页面中存在的问题！1. 关于宽窄版页面的实现。ie9以及以上、ff, chrome我是采用media query来实现宽窄屏，ie8及以下通过引入js来判断调整。我想尝试在浏览器resize的时候就重新设置一下版面的布局，但是未果。所以ie8及以下的浏览器只得重新刷新页面才可以实现宽窄版布局的切换。2. 轮播图。我观察了一下我实现的轮播图，发现只有第一个图有淡入淡出效果，第二第三个图有淡出效果没有淡入效果。而最后一个图感觉淡入淡出都没有●﹏●，我尝试了很多次但是还没有找到解决问题的办法。');
 
 
 		});
